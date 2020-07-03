21fbda2d18f0b6d2e8656a4aeff2331acdb6ba33
José Assis de Menezes Neto <netomenezesucg@gmail.com>
2020-07-03 09:02:00 -0300

    Finalizado primeiro desafio do nível 3

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 
 import filesize from 'filesize';
 
@@ -7,59 +7,71 @@ import Header from '../../components/Header';
 import FileList from '../../components/FileList';
 import Upload from '../../components/Upload';
 
-import { Container, Title, ImportFileContainer, Footer } from './styles';
+import {Container, Title, ImportFileContainer, Footer} from './styles';
 
 import alert from '../../assets/alert.svg';
 import api from '../../services/api';
 
 interface FileProps {
-  file: File;
-  name: string;
-  readableSize: string;
+    file: File;
+    name: string;
+    readableSize: string;
 }
 
 const Import: React.FC = () => {
-  const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
-  const history = useHistory();
+    const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
+    const history = useHistory();
 
-  async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    async function handleUpload(): Promise<void> {
+        const data = new FormData();
 
-    // TODO
+        if (!uploadedFiles.length) {
+            return;
+        }
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
+        const file = uploadedFiles[0];
+
+        data.append('file', file.file, file.name);
+
+        try {
+            await api.post('/transactions/import', data);
+            history.push('/');
+        } catch (err) {
+            console.log(err.response.error);
+        }
     }
-  }
 
-  function submitFile(files: File[]): void {
-    // TODO
-  }
+    function submitFile(files: File[]): void {
+        const uploadFiles = files.map(file => ({
+            file,
+            name: file.name,
+            readableSize: filesize(file.size)
+        }));
+        setUploadedFiles(uploadFiles);
+    }
 
-  return (
-    <>
-      <Header size="small" />
-      <Container>
-        <Title>Importar uma transação</Title>
-        <ImportFileContainer>
-          <Upload onUpload={submitFile} />
-          {!!uploadedFiles.length && <FileList files={uploadedFiles} />}
+    return (
+        <>
+            <Header size="small"/>
+            <Container>
+                <Title>Importar uma transação</Title>
+                <ImportFileContainer>
+                    <Upload onUpload={submitFile}/>
+                    {!!uploadedFiles.length && <FileList files={uploadedFiles}/>}
 
-          <Footer>
-            <p>
-              <img src={alert} alt="Alert" />
-              Permitido apenas arquivos CSV
-            </p>
-            <button onClick={handleUpload} type="button">
-              Enviar
-            </button>
-          </Footer>
-        </ImportFileContainer>
-      </Container>
-    </>
-  );
+                    <Footer>
+                        <p>
+                            <img src={alert} alt="Alert"/>
+                            Permitido apenas arquivos CSV
+                        </p>
+                        <button onClick={handleUpload} type="button">
+                            Enviar
+                        </button>
+                    </Footer>
+                </ImportFileContainer>
+            </Container>
+        </>
+    );
 };
 
 export default Import;
--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,4 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL', currencyDisplay: 'symbol', minimumFractionDigits: 2}).format(value); // TODO
 
 export default formatValue;
